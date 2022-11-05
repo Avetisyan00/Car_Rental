0fcbaf5ce78b16c3870ee883bd7c02b1f8bab296
Levon sahakyan <lsahakyan074@gmail.com>
2022-11-05 14:38:55 -0500

    Created controllers (add, remove, show, edit...), created pagination, organized images, fixed db and changed production year type to INT(4)

--- src/main/java/com/example/carrental/controller/MainController.java
+++ src/main/java/com/example/carrental/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.example.carrental.controller;
 
+import com.example.carrental.entity.Car;
+import com.example.carrental.service.CarService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -10,12 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final CarService carService;
+
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(ModelMap modelMap) {
+        List<Car> cars = carService.findAll();
+        modelMap.addAttribute("cars",cars);
         return "index";
     }
 
